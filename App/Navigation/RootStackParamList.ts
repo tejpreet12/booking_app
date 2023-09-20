ca973ecc870c87c441b6a7bcaa5e9fd44f310c71
tejpreet12 <tejpreetdhiman429@gmail.com>
2023-09-21 00:39:24 +0530

    Navigation

---
+++ App/Navigation/RootStackParamList.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList = {
+
+    TabNavigation : undefined,
+    HomeScreen:undefined,
+    BookingScreen:undefined,
+    ProfileScreen:undefined,
+    SavedScreen:undefined,
+
+
+}
